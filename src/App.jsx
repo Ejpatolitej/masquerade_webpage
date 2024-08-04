8c77270387bc3d23987eb4648add491432643395
Ejpatolitej <niklassonjack@gmail.com>
2024-08-04 15:54:21 +0200

    init dev commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import mask from './assets/blackGoldMask.jpg';
+
 function App() {
   return (
     <>
-      <div className='announcement'>
-        <h1>COMING SOON...</h1>
+      <div>
+        <img className='bannerimg' src={mask} alt='Logo' />;{' '}
       </div>
+      <h1 className='announcement'>COMING SOON...</h1>
     </>
   );
 }

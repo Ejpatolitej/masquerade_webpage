82f46e13a352c2d7c971a96589a073b1d02ed469
ejpatolitej <niklassonjack@gmail.com>
2024-08-07 20:02:59 +0200

    InfoText

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ function App() {
         <h1 className='hero-text'>Autumn</h1>
         <h1 className='hero-text'>Masquerade</h1>
       </div>
+      <div>
+        <h1 className='info-text'>15e Oktober | Rådhuset</h1>
+      </div>
       <h1 className='announcement'>COMING SOON...</h1>
     </>
   );
